0afb3795b0b8b3b65e81d3a57f894ed827c211af
MarcosAndresCarreras <marcosandrescarreras@gmail.com>
2017-09-02 17:56:09 -0300

    classRelationship

--- Entidades/classRelationship.cs
+++ Entidades/classRelationship.cs
@@ -7,6 +7,41 @@ namespace Entidades
 {
     public class classRelationship
     {
+         #region Atributos y Metodos
 
+        public int IdRelationship { set; get; }
+        public string Description { set; get; }
+        public bool Visible { set; get; }
+        
+        #endregion
+
+        #region Constructores
+
+        public classRelationship()
+        {
+            this.IdRelationship = 0;
+            this.Description = string.Empty;
+            this.Visible = true;
+        }
+
+        public classRelationship(int vIdRelationship, string vDescription, bool Visible)
+        {
+            this.IdRelationship = vIdRelationship;
+            this.Description = vDescription;
+            this.Visible = Visible;
+        }
+
+        #endregion
+
+        #region Metodos
+        public string toString()
+        {
+            return
+            "Id: " + this.IdRelationship +
+            "\nRelación: " + this.Description +
+            "\nVisible: " + this.Visible;
+        }
+
+        #endregion
     }
 }
