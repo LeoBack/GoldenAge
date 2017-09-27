6f5ca362de2c00b1cf1ba2eff6653abea963ffb3
MarcosAndresCarreras <marcosandrescarreras@gmail.com>
2017-09-26 21:54:39 -0300

    classIvaType
    Entidad classIvaType

---
+++ Entidades/Clases/classIvaType.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entidades.Clases
+{
+    public class classIvaType
+    {
+        #region Atributos y Metodos
+
+        public int IdIvaType { set; get; }
+        public string Description { set; get; }
+        public bool Visible { set; get; }
+
+        #endregion
+
+        #region Constructores
+
+        public classIvaType()
+        {
+            IdIvaType = 0;
+            Description = string.Empty;
+            Visible = true;
+        }
+
+        public classIvaType(int vIdIvaType)
+        {
+            IdIvaType = vIdIvaType;
+            Description = string.Empty;
+            Visible = true;
+        }
+
+        public classIvaType(int vIdIvaType, string vDescription, bool vVisible)
+        {
+            IdIvaType = vIdIvaType;
+            Description = vDescription;
+            Visible = vVisible;
+        }
+
+        #endregion
+
+        #region Metodos
+        public override string ToString()
+        {
+            return
+            "IdIvaType: " + IdIvaType.ToString() +
+            "\nDescription: " + Description +
+            "\nVisible: " + Visible.ToString();
+        }
+
+        #endregion
+
+    }
+}
